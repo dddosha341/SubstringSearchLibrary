79f8dc50055fcb470a36b36bde5f276422334ee2
Dmitry Voropaev <workerv0ropaev@yandex.ru>
2025-04-23 21:41:28 +0300

    added an implementation of the ISubstringSearch interface for the brute-force algorithm

--- SubstringSearchLibrary/Algorithms/BruteForceAlgorithm.cs
+++ SubstringSearchLibrary/Algorithms/BruteForceAlgorithm.cs
@@ -6,7 +6,27 @@ namespace SubstringSearchLibrary.Algorithms
     {
         public IEnumerable<int> IndexesOf(string text, string pattern)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(pattern) || pattern.Length > text.Length)
+                return Enumerable.Empty<int>();
+
+            var result = new List<int>();
+            int textLength = text.Length;
+            int patternLength = pattern.Length;
+
+            for (int i = 0; i <= textLength - patternLength; i++)
+            {
+                int j;
+                for (j = 0; j < patternLength; j++)
+                {
+                    if (text[i + j] != pattern[j])
+                        break;
+                }
+
+                if (j == patternLength)
+                    result.Add(i);
+            }
+
+            return result;
         }
     }
 }
