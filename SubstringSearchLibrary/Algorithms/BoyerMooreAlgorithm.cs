307c61e655a23efb1ac55e881f1a8a7a7df38f2e
Dmitry Voropaev <workerv0ropaev@yandex.ru>
2025-04-23 21:40:09 +0300

    added an implementation of the ISubstringSearch interface for the Boyer-Moore algorithm

--- SubstringSearchLibrary/Algorithms/BoyerMooreAlgorithm.cs
+++ SubstringSearchLibrary/Algorithms/BoyerMooreAlgorithm.cs
@@ -6,7 +6,82 @@ namespace SubstringSearchLibrary.Algorithms
     {
         public IEnumerable<int> IndexesOf(string text, string pattern)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(pattern) || pattern.Length > text.Length)
+                return Enumerable.Empty<int>();
+
+            var badCharTable = BuildBadCharTable(pattern);
+            var goodSuffixTable = BuildGoodSuffixTable(pattern);
+
+            var result = new List<int>();
+            int textLength = text.Length;
+            int patternLength = pattern.Length;
+            int shift = 0;
+
+            while (shift <= textLength - patternLength)
+            {
+                int j = patternLength - 1;
+
+                while (j >= 0 && pattern[j] == text[shift + j])
+                    j--;
+
+                if (j < 0)
+                {
+                    result.Add(shift);
+                    shift += goodSuffixTable[0];
+                }
+                else
+                {
+                    shift += Math.Max(goodSuffixTable[j], j - badCharTable[text[shift + j]]);
+                }
+            }
+
+            return result;
+        }
+
+        private int[] BuildBadCharTable(string pattern)
+        {
+            const int alphabetSize = 256; // ASCII
+            var table = new int[alphabetSize];
+            for (int i = 0; i < alphabetSize; i++)
+                table[i] = -1;
+
+            for (int i = 0; i < pattern.Length; i++)
+                table[pattern[i]] = i;
+
+            return table;
+        }
+
+        private int[] BuildGoodSuffixTable(string pattern)
+        {
+            int length = pattern.Length;
+            var table = new int[length];
+            var borderPos = new int[length + 1];
+            int i = length, j = length + 1;
+            borderPos[i] = j;
+
+            while (i > 0)
+            {
+                while (j <= length && pattern[i - 1] != pattern[j - 1])
+                {
+                    if (table[j] == 0)
+                        table[j] = j - i;
+                    j = borderPos[j];
+                }
+                i--;
+                j--;
+                borderPos[i] = j;
+            }
+
+            j = borderPos[0];
+            for (i = 0; i <= length; i++)
+            {
+                if (table[i] == 0)
+                    table[i] = j;
+                if (i == j)
+                    j = borderPos[j];
+            }
+
+            return table;
         }
     }
 }
